8f21128ad9fbae6c04246bcb4a22f25445841aae
Orlando Romo <orlando.romo.97@hotmail.com>
2022-02-07 23:12:12 -0600

    feature: Define needed Repository interface methods in the repos package to create a user and get a user by id

--- repos/postgres/postgres.go
+++ repos/postgres/postgres.go
@@ -1,27 +1,57 @@
 package postgres
 
 import (
-	"fmt"
+	"context"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/jmoiron/sqlx"
-	"github.com/spf13/viper"
+	"github.com/orlandorode97/mailx-google-service/repos"
 )
 
-type Repository struct {
+type repository struct {
 	db *sqlx.DB
 }
 
-func BuildDSN() string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%d/?sslmode=%s",
-		viper.GetString("POSTGRES_USER"),
-		viper.GetString("POSTGRES_PASSWORD"),
-		viper.GetString("POSTGRES_HOST"),
-		viper.GetInt("POSTGRES_PORT"),
-		viper.GetString("POSTGRES_SSL_MODE"))
-}
-
-func New(db *sqlx.DB) *Repository {
-	return &Repository{
+func New(db *sqlx.DB) repos.Repository {
+	return &repository{
 		db: db,
 	}
+}
+
+func (r *repository) CreateUser(ctx context.Context, user *repos.User) error {
+	query, args, err := sq.
+		Insert("users").
+		Columns("google_id", "name", "given_name", "family_name", "picture", "locale").
+		Values(user.ID, user.Name, user.GivenName, user.FamilyName, user.Picture, user.Locale).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return err
+	}
+
+	if _, err = r.db.DB.ExecContext(ctx, query, args...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *repository) GetUserByID(ctx context.Context, ID string) (*repos.User, error) {
+	var user repos.User
+	query, args, err := sq.
+		Select("google_id", "name", "given_name", "family_name", "picture", "locale").
+		From("users").
+		Where("googl_id = $1", ID).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	row := r.db.DB.QueryRowContext(ctx, query, args...)
+	if err = row.Scan(&user.ID, &user.Name, &user.GivenName, &user.FamilyName, &user.Picture, &user.Locale); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
--- repos/repos.go
+++ repos/repos.go
@@ -1,3 +1,33 @@
 package repos
 
-type Repository interface{}
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type User struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	GivenName  string `json:"given_name"`
+	FamilyName string `json:"family_name"`
+	Picture    string `json:"picture"`
+	Locale     string `json:"locale"`
+}
+
+func BuildDSN() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s&search_path=%s",
+		viper.GetString("POSTGRES_USER"),
+		viper.GetString("POSTGRES_PASSWORD"),
+		viper.GetString("POSTGRES_HOST"),
+		viper.GetInt("POSTGRES_PORT"),
+		viper.GetString("POSTGRES_DB_NAME"),
+		viper.GetString("POSTGRES_SSL_MODE"),
+		viper.GetString("POSTGRES_DB_SCHEMA"))
+}
+
+type Repository interface {
+	CreateUser(context.Context, *User) error
+	GetUserByID(context.Context, string) (*User, error)
+}
