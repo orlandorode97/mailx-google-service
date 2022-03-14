fdacbbbaa08f3425cf1e60c282e4dc72f8a50883
Orlando Romo <34588445+orlandorode97@users.noreply.github.com>
2022-03-14 00:40:46 -0600

    tests: Add unit test for RecreateGmailService function (#16)

--- service_test.go
+++ service_test.go
@@ -1,10 +1,14 @@
 package mailx
 
 import (
+	"context"
+	"errors"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/go-kit/log"
+	"github.com/orlandorode97/mailx-google-service/pkg/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"golang.org/x/oauth2"
@@ -13,6 +17,35 @@ import (
 
 type MockMailxService struct {
 	mock.Mock
+}
+
+type MockDB struct {
+	mock.Mock
+}
+
+func (db MockDB) CreateUser(ctx context.Context, user *models.User) error {
+	args := db.Called(ctx, user)
+	return args.Error(0)
+}
+
+func (db MockDB) GetUserByID(ctx context.Context, ID string) (*models.User, error) {
+	args := db.Called(ctx, ID)
+	return args.Get(0).(*models.User), args.Error(1)
+}
+
+func (db MockDB) GetTokenByUserId(ctx context.Context, ID string) (*models.Token, error) {
+	args := db.Called(ctx, ID)
+	return args.Get(0).(*models.Token), args.Error(1)
+}
+
+func (db MockDB) SaveAccessToken(ctx context.Context, ID string, token *oauth2.Token) error {
+	args := db.Called(ctx, ID, token)
+	return args.Error(0)
+}
+
+func (db MockDB) UpdateAccessToken(ctx context.Context, ID string, token *oauth2.Token) error {
+	args := db.Called(ctx, ID, token)
+	return args.Error(0)
 }
 
 func TestAddGmailServiceByID(t *testing.T) {
@@ -84,4 +117,66 @@ func TestCreateGmailService(t *testing.T) {
 		assert.Nil(t, err)
 		assert.NotNil(t, gmailSvc)
 	})
+}
+
+func TestRecreateGmailService(t *testing.T) {
+	testcases := []struct {
+		name      string
+		ctx       context.Context
+		userId    string
+		token     *models.Token
+		errToken  error
+		errSvc    error
+		assertErr func(t assert.TestingT, object interface{}, msgAndArgs ...interface{}) bool
+		assertSvc func(t assert.TestingT, object interface{}, msgAndArgs ...interface{}) bool
+	}{
+		{
+			name:   "success - gmail service recreated",
+			ctx:    context.Background(),
+			userId: "1",
+			token: &models.Token{
+				AccessToken:     "access-token",
+				RefreshToken:    "refresh-token",
+				TokenType:       "Bearer",
+				TokenExpiration: time.Now(),
+			},
+			assertErr: assert.Nil,
+			assertSvc: assert.NotNil,
+		},
+		{
+			name:      "failure - get token by id fails",
+			ctx:       context.Background(),
+			userId:    "1",
+			token:     nil,
+			errToken:  errors.New("database is not online"),
+			assertErr: assert.NotNil,
+			assertSvc: assert.Nil,
+		},
+		{
+			name:   "failure - gmail cannot be recreated",
+			ctx:    context.Background(),
+			userId: "1",
+			token: &models.Token{
+				AccessToken:     "access-token",
+				RefreshToken:    "refresh-token",
+				TokenType:       "Bearer",
+				TokenExpiration: time.Now(),
+			},
+			errSvc:    errors.New("token no valid"),
+			assertErr: assert.Nil,
+			assertSvc: assert.NotNil,
+		},
+	}
+
+	for _, test := range testcases {
+		t.Run(test.name, func(t *testing.T) {
+			db := MockDB{}
+			db.On("GetTokenByUserId", test.ctx, test.userId).Return(test.token, test.errToken)
+			logger := log.NewLogfmtLogger(os.Stdout)
+			svc := New(logger, db, nil)
+			newSvc, err := svc.RecreateGmailService(test.ctx, test.userId)
+			test.assertErr(t, err)
+			test.assertSvc(t, newSvc)
+		})
+	}
 }
